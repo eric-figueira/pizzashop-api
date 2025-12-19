f4f04430bd07763c02426311e39839f7ccba16f7
Éric Carvalho Figueira <eric.figueira.07@gmail.com>
2025-12-19 09:55:07 -0300

    chore: config environment variable

---
+++ src/env.ts
@@ -0,0 +1,10 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
+import { z } from 'zod'
+
+const envSchema = z.object({
+  DATABASE_URL: z.url().min(1),
+})
+
+export const env = envSchema.parse(process.env)
