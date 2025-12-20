2a2a7028ea9b15e0eb8e1b844ad32fae797021b6
Éric Carvalho Figueira <eric.figueira.07@gmail.com>
2025-12-20 13:55:50 -0300

    feat: create auth link table

--- src/db/schema/restaurants.ts
+++ src/db/schema/restaurants.ts
@@ -4,14 +4,20 @@ import { users } from './users'
 import { relations } from 'drizzle-orm'
 
 export const restaurants = pgTable('restaurants', {
-  id: text('id').$defaultFn(() => createId()).primaryKey(),
+  id: text('id')
+    .$defaultFn(() => createId())
+    .primaryKey(),
   name: text('name').notNull(),
   description: text('description'),
   managerId: text('manager_id').references(() => users.id, {
     onDelete: 'set null'
   }),
-  createdAt: timestamp('created_at').notNull().defaultNow(),
-  updatedAt: timestamp('updated_at').notNull().defaultNow(),
+  createdAt: timestamp('created_at')
+    .notNull()
+    .defaultNow(),
+  updatedAt: timestamp('updated_at')
+    .notNull()
+    .defaultNow(),
 })
 
 
--- src/db/schema/users.ts
+++ src/db/schema/users.ts
@@ -4,11 +4,19 @@ import { createId } from '@paralleldrive/cuid2'
 export const userRoleEnum = pgEnum('user_role', ['manager', 'customer'])
 
 export const users = pgTable('users', {
-	id: text('id').$defaultFn(() => createId()).primaryKey(),
+	id: text('id')
+    .$defaultFn(() => createId())
+    .primaryKey(),
 	name: text('name').notNull(),
-	email: text('email').notNull().unique(),
+	email: text('email')
+    .notNull()
+    .unique(),
   phone: text('phone'),
   role: userRoleEnum('role').default('customer').notNull(),
-  createdAt: timestamp('created_at').notNull().defaultNow(),
-  updatedAt: timestamp('updated_at').notNull().defaultNow(),
+  createdAt: timestamp('created_at')
+    .notNull()
+    .defaultNow(),
+  updatedAt: timestamp('updated_at')
+    .notNull()
+    .defaultNow(),
 });
