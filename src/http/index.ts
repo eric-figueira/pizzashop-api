98208d47bb59ebcfb2fdf32334613675ea490b22
Éric Carvalho Figueira <eric.figueira.07@gmail.com>
2025-12-20 13:26:47 -0300

    feat: add insert restaurant route

--- src/http/index.ts
+++ src/http/index.ts
@@ -1,7 +1,43 @@
-import express from 'express';
+import express from 'express'
+import { db } from '../db/connection'
+import { restaurants, users } from '../db/schema'
+import z from 'zod'
+import { validate } from './middlewares/request-body-validator'
 
-const app = express();
+const app = express()
+
+const insertRestaurantSchema = z.object({
+  restaurantName: z.string().nonempty(),
+  managerName: z.string().nonempty(),
+  email: z.email(),
+  phone: z.string().nullable(),
+})
+
+type InsertRestaurantDTO = z.infer<typeof insertRestaurantSchema>
+
+app.post('/restaurants', validate(insertRestaurantSchema), async (req, res) => {
+  const { restaurantName, managerName, email, phone } = req.body as InsertRestaurantDTO
+
+  const [manager] = await db
+    .insert(users)
+    .values({
+      name: managerName,
+      email,
+      phone,
+      role: 'manager'
+    })
+    .returning({ 
+      id: users.id 
+    })
+
+  await db.insert(restaurants).values({
+    name: restaurantName,
+    managerId: manager!.id
+  })
+
+  res.status(204)
+})
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
-});
+})
