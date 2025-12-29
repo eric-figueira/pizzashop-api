1dd8b2b9c7a95a5cee26018469c115bd9eaef58f
Éric Carvalho Figueira <eric.figueira.07@gmail.com>
2025-12-29 15:09:31 -0300

    refactor: send sign out request

--- src/http/routes/sign-out.ts
+++ src/http/routes/sign-out.ts
@@ -3,5 +3,6 @@ import type { Router } from 'express'
 export const setUpSignOutRoute = (router: Router) => {
   router.post('/sign-out', (req, res) => {
     res.clearCookie('auth')
+    res.send()
   })
 }
